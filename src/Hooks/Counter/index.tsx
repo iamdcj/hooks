ebeca953ef737f8249b6c59d81ebabd351e7ff40
David Christian Jones <david@dcj.codes>
2020-02-14 10:58:57 -0500

    improve counter intro

--- src/Hooks/Counter/index.tsx
+++ src/Hooks/Counter/index.tsx
@@ -8,6 +8,10 @@ const Counter: React.SFC<CounterProps> = () => {
   return (
     <div>
       <h1>Counter</h1>
+      <p>The following effects are used in this example:</p>
+      <ul>
+        <li>useState - this handles the state for the count value.</li>
+      </ul>
       <span>{count}</span>
       <button onClick={() => updateCount(count + 1)}>+</button>
       <button onClick={() => updateCount(count - 1)}>-</button>
