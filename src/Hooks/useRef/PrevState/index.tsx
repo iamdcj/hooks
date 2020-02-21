42bb7b0b611afad2a2f9a7f1a96f5ac951fbb800
David Christian Jones <david@dcj.codes>
2020-02-20 20:04:32 -0500

    usePrevious

--- src/Hooks/useRef/PrevState/index.tsx
+++ src/Hooks/useRef/PrevState/index.tsx
@@ -19,6 +19,10 @@ const PrevState: React.SFC<DOMProps> = () => {
         <li>
           useRef - this captures the previous version of the count variable.
         </li>
+        <li>
+          useEffect - this the capturing of previous versio of the count
+          variable with every update.
+        </li>
       </ul>
       <p>
         Current Count: {count} | Previous Count:{prevState.current}
